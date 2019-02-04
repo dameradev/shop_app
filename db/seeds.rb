480da3334929f132c1220b134e2f4e71b1327322
=DamjanRadev <radevdamjan@gmail.com>
2019-02-04 13:27:35 +0100

    Created sample data with seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(name: "Banana", price: 0.49, active: true)
+Product.create!(name: "Apple", price: 0.29, active: true)
+Product.create!(name: "Carton of Strawberries", price: 1.99, active: true)
+
+puts "3 products created"
+
+OrderStatus.create!(name: "In Progress")
+OrderStatus.create!(name: "Placed")
+OrderStatus.create!(name: "Shipped")
+OrderStatus.create!(name: "Cancelled")
+
+puts "4 OrderStatuses created"
