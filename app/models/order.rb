caf092448b0791c42b9814c3d421d95134bdf28a
=DamjanRadev <radevdamjan@gmail.com>
2019-02-04 14:03:43 +0100

    Added comments to explain the code in the models

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,17 +4,23 @@ class Order < ApplicationRecord
   before_create :set_order_status
   before_save :update_subtotal
 
+  #overides the filed subtotal
+  #order.subtotal calls the function
   def subtotal
     order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
   end
 
   private
 
+  # when order is created to change status
   def set_order_status
     self.order_status_id = 1
   end
 
+  #when order is saved to update the subtotal
+  #sums up total cost
   def update_subtotal
+    #this is how we update the field subtotal
     self[:subtotal] = subtotal
   end
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,12 +2,17 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+  #ensures that we have more than 0 product(s)
   validates :quantity, presence:true, numericality: { only_integer: true, greater_than: 0 }
+  #custom validations
   validate :product_present
   validate :order_present
 
   before_save :finalize
 
+  #if the order_item doesn't yet exist we take the assosiated product's unit_price
+  #elseif the order_item does exist the contents of the unit price will be returned instad
+  # This means that once the product is added to our cart, if the product changes price the user will still be able to buy the product at the previous price.
   def unit_price
     if persisted?
       self[:unit_price]
@@ -33,6 +38,7 @@ class OrderItem < ApplicationRecord
     end
   end
 
+  #updates the unit and total price with the proper values
   def finalize
     self[:unit_price] = unit_price
     self[:total_price] = quantity * self[:unit_price]
